85790bf5aee62dd1f0fc97ddd1710bb33185f446
zhaozheng7 <zhaozheng96@outlook.com>
2026-03-25 16:09:36 +0800

    plat-d06: Add support for HIP08A
    HIP08A is another form of the D06 development board and equipped by
    Hisilicon.
    
    Signed-off-by: zhaozheng <zhaozheng30@huawei.com>

--- core/arch/arm/plat-d06/main.c
+++ core/arch/arm/plat-d06/main.c
@@ -5,16 +5,31 @@
  */
 #include <console.h>
 #include <drivers/lpc_uart.h>
+#include <drivers/pl011.h>
 #include <platform_config.h>
 
+#if defined(PLATFORM_FLAVOR_d06)
 static struct lpc_uart_data console_data __nex_bss;
+#endif
+
+#if defined(PLATFORM_FLAVOR_HIP08A)
+static struct pl011_data console_data __nex_bss;
+#endif
 
 register_phys_mem_pgdir(MEM_AREA_IO_NSEC, LPC_BASE, LPC_SIZE);
+register_phys_mem_pgdir(MEM_AREA_IO_SEC, SEC_BASE, SEC_SIZE);
+register_phys_mem_pgdir(MEM_AREA_IO_SEC, HISI_TRNG_BASE, HISI_TRNG_SIZE);
 
 void plat_console_init(void)
 {
+#if defined(PLATFORM_FLAVOR_d06)
 	lpc_uart_init(&console_data, LPC_BASE, CONSOLE_UART_CLK_IN_HZ,
 		      CONSOLE_BAUDRATE);
+#endif
 
+#if defined(PLATFORM_FLAVOR_HIP08A)
+	pl011_init(&console_data, UART_BASE, CONSOLE_UART_CLK_IN_HZ,
+		      CONSOLE_BAUDRATE);
+#endif
 	register_serial_console(&console_data.chip);
 }
